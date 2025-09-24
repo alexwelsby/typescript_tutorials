38571adc6639d27878c051aced8fe50a17dc163b
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-09-24 23:05:32 +1200

    initial commit
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

---
+++ src/components/Person.tsx
@@ -0,0 +1,8 @@
+import type { PersonProps } from "./Person.types";
+export const Person = (props: PersonProps) => {
+  return (
+    <div>
+      {props.name.first} {props.name.last}
+    </div>
+  );
+};
